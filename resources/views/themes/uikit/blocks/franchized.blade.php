63bead6ed196d736c2110f6454eea0f7370faf9c
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-11-10 20:12:42 +0200

    змінив адресу кнопки в блоці franchize

--- resources/views/themes/uikit/blocks/franchized.blade.php
+++ resources/views/themes/uikit/blocks/franchized.blade.php
@@ -1,6 +1,6 @@
 <div class="franchise">
     <div class="container">
         <div class="franchise__text">Looking for your region franchise and be region head guide?</br></div>
-            <a href="{{route('contact')}}" class="btn btn-white">complete a form</a>
+            <a href="{{route('franchize')}}" class="btn btn-white">in detail</a>
     </div>
 </div>
