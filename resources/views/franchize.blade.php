403b4981a9b6108be4e8dd3043f7f9bc2d86e623
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-11-24 21:50:49 +0200

    Забрав 2 заголовок на Франшиза

--- resources/views/franchize.blade.php
+++ resources/views/franchize.blade.php
@@ -10,7 +10,7 @@
 <div class="franchize-container">
     <div class="container">
         
-        <h3 class="franch__title">  Franchise</h2>
+        {{-- <h3 class="franch__title">  Franchise</h2> --}}
             <p>
                 You can make a region franchise request - the region choice is not related to the country and city where you are located. The most important condition is that you must be well oriented in chosen region.
 The franchise cost depends on the selected country and region, and ranges from 1500 EUR to 5000 EUR for a 3 months period. After three months, a franchise renewal is required in the established amount for the next three months of region franchise using.
