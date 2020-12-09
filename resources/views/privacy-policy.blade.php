e7e418b8a979c9f85e384d35728826a9935a706b
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-12-09 23:27:25 +0200

    Privacy and terms Оновив

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -13,22 +13,96 @@
 <div class="guest-info">
     <div class="container">
         <div class="guest-info__text">
-           
-​           <p>I’m a privacy policy section. I’m a great place to inform your visitors about how you use, store, and protect their personal information. Add details such as what information you collect, whether it be email addresses,
-            names or phone numbers, why you collect this information, and how you use it.
-            ​</p>
-            <p class="guest-info__paragraph">Your user’s privacy is of the highest importance to your organization, so take the time to write an accurate and detailed policy.
-                 Use straightforward language to gain their trust and make sure they keep coming back to your site!
-            </p> 
+                    
+            <p>​At Jet7One, your privacy is a top priority. Your privacy is at the core of the way we design and build the services and products you know and love, so that you can fully trust us and focus on building meaningful connections.</p>
+            <p>We appreciate that you put your trust in us when you provide us with your information and we do not take this lightly.</p>
+            <p>We do not compromise with your privacy. We design all of our products and services with your privacy in mind. </p>
+                We involve experts from various fields, including legal, security, engineering, product design and others to make sure that no decision is taken without respect for your privacy.</p>
+            <p>We strive to be transparent in the way we process your data. Because we use many of the same online services you do, we know that insufficient information and overly complicated language are common issues in privacy policies.</p>
+                 We take the exact opposite approach: we have written our Privacy Policy and related documents in plain language. We actually want you to read our policies and understand our privacy practices!</p>
+            <p>We constantly update our security practices and invest in our security efforts to enhance the safety of your information.</p>
+            <h2>Privacy Policy</h2>
+            <p>Welcome to Jet7One’s Privacy Policy. Thank you for taking the time to read it.</p>
+            <p>We appreciate that you trust us with your information and we intend to always keep that trust. This starts with making sure you understand the information we collect, why we collect it.
+            <p>This Privacy Policy applies beginning May 25, 2018. </p>
+            <p><span class="text-bottom">1. Where This Privacy Policy Applies</span></p>
+            <p>This Privacy Policy applies to websites, apps, events and other services operated by Jet7One. For simplicity, we refer to all of these as our “services” in this Privacy Policy. </p>
+            <p>Some services may require their own unique privacy policy. If a particular service has its own privacy policy, then that policy - not this Privacy Policy - applies.</p>
+            <p><span class="text-bottom">2. Collecting Information</span></p>
+            <p>It goes without saying, we can’t help you develop meaningful connections without some information about you, such as basic profile guide details you’d like to see. We also collect information generated as you use our services, for example access logs. </p>
+            <p>Information you give us</p>
+            <p>You choose to give us certain information when using our services. When you create an account, you provide us with at least your login credentials, as well as some basic details necessary for the service to work. When you complete your guide profile, you can share with us additional information, such as details on your personality, lifestyle, interests and other details about you, as well as content such as photos. Some of the information you choose to provide us may be considered “special” or “sensitive” in certain jurisdictions, for example your racial or ethnic origins. By choosing to provide this information, you consent to our processing of that information.</p>
+            <p>When you subscribe to a paid service or make a purchase directly from us, you provide us or our payment service provider with information, such as your debit or credit card number or other financial information.</p>
+            <p>When you choose to participate in our events or hot tours, we collect the information that you use to register or enter.</p>
+            <p>If you contact our customer care team, we collect the information you give us during the interaction. Sometimes, we monitor or record these interactions for training purposes and to ensure a high quality of service.</p>
+            <p>If you ask us to communicate with or otherwise process information of other people (for example, if you ask us to send an email on your behalf to one of your friends), we collect the information about others that you give us in order to complete your request.</p>
+            <p>Of course, we also process your chats with other users as well as the content you publish, as part of the operation of the services.</p>
+            <p>Information we receive from others</p>
+            <p>In addition to the information you provide us directly, we receive information about you from others.</p>
+            <p>Other Users </p>
+            <p>Other users may provide information about you as they use our services. For instance, we may collect information about you from other users if they contact us about you.</p>
+            <p>Other Partners</p>
+            <p>We may receive info about you from our partners, for instance where Jet7One ads are published on a partner’s websites and platforms (in which case they may pass along details on a campaign’s success).</p>
+            <p>Information collected when you use our services</p>
+            <p>When you use our services, we collect information about which features you’ve used, how you’ve used them.</p>
+            <p>Usage Information</p>
+            <p>We collect information about your activity on our services, for instance how you use them (e.g., date and time you logged in, features you’ve been using, searches, clicks and pages which have been shown to you, referring webpage address, advertising that you click on) and how you interact with other users (e.g., users you connect and interact with, time and date of your exchanges, number of messages you send and receive).</p>
+            <p>Other information with your consent</p>
+            <p>If you give us permission, we can collect your precise geolocation (latitude and longitude) through various means, depending on the service and device you’re using, including GPS, Bluetooth or Wi-Fi connections. The collection of your geolocation may occur in the background even when you aren’t using the services if the permission you gave us expressly permits such collection. If you decline permission for us to collect your geolocation, we will not collect it. Similarly, if you consent, we may collect your photos (for instance, if you want to publish a photo).</p>
+            <p><span class="text-bottom">3. Cookies and Other Similar Data Collection Technologies</span></p>
+            <p>We use and may allow others to use cookies and similar technologies to recognize you and/or your device(s). We use them for authenticating you, remembering your preferences and settings, analyzing site traffic and trends, delivering and measuring the effectiveness of advertising campaigns and how you can better control their use, through your browser settings and other tools.</p>
+            <p>Some web browsers (including Safari, Internet Explorer, Firefox and Chrome) have a “Do Not Track” (“DNT”) feature that tells a website that a user does not want to have his or her online activity tracked. If a website that responds to a DNT signal receives a DNT signal, the browser can block that website from collecting certain information about the browser’s user. Not all browsers offer a DNT option and DNT signals are not yet uniform. For this reason, many businesses, including Jet7One, do not currently respond to DNT signals.</p>
+            <p><span class="text-bottom">4. How We Use Information</span></p>
+            <p>The main reason we use your information is to deliver and improve our services. Additionally, we use your info to help keep you safe and to provide you with advertising that may be of interest to you. 
+            <p>Most of the time, the reason we process your information is to perform the contract that you have with us. For instance, as you go about using our service to build connections, we use your information to maintain your account and your profile, to make it viewable to other users.
+            <p>We may use your information where we have legitimate interests to do so. For instance, we analyze users’ behavior on our services to continuously improve our offerings, we suggest offers we think might interest you, and we process information for administrative, fraud detection and other legal purposes.
+            <p>Sometimes we may ask for your consent to use your information for certain specific reasons.</p>
+            <p><span class="text-bottom">5. How We Share Information</span></p>
+            <p>Since our goal is to help you make connections and provide for you best quide fashion models, the main sharing of users’ information is, of course, with other users. We also share some users’ information with service providers and partners who assist us in operating the services.</p>
+           <p> With other users</p>
+           <p> You share information with other users when you voluntarily disclose information on the service (including your public profile). Please be careful with your information and make sure that the content you share is stuff that you’re comfortable being publically viewable since neither you nor we can control what others do with your information once you share it.</p>
+           <p>With our service providers and partners</p>
+            <p>We use third parties to help us operate and improve our services. These third parties assist us with various tasks, including data hosting and maintenance, analytics, customer care, marketing, advertising, payment processing and security operations.</p><p>
+            <p>We may also share information with partners who distribute and assist us in advertising our services. For instance, we may share limited information on you in hashed, non-human readable form to advertising partners.</p>
+            <p>We follow a strict vetting process prior to engaging any service provider or working with any partner. All of our service providers and partners must agree to strict confidentiality obligations.</p>
+            <p>When required by law</p>
+            <p>We may disclose your information if reasonably necessary: (i) to comply with a legal process, such as a court order, subpoena or search warrant, government / law enforcement investigation or other legal requirements; (ii) to assist in the prevention or detection of crime (subject in each case to applicable law); or (iii) to protect the safety of any person.
+            <p>To enforce legal rights</p>
+            <p>We may also share information: (i) if disclosure would mitigate our liability in an actual or threatened lawsuit; (ii) as necessary to protect our legal rights and legal rights of our users, business partners or other interested parties; (iii) to enforce our agreements with you; and (iv) to investigate, prevent, or take other action regarding illegal activity, suspected fraud or other wrongdoing.
+            <p>With your consent or at your request</p>
+            <p>We may ask for your consent to share your information with third parties. In any such case, we will make it clear why we want to share the information.</p>
+            <p>We may use and share non-personal information (meaning information that, by itself, does not identify who you are such as device information, general demographics, general behavioral data, geolocation in de-identified form), as well as personal information in hashed, non-human readable form, under any of the above circumstances. We may combine this information with additional non-personal information or personal information in hashed, non-human readable form collected from other sources. More information on our use of cookies and similar technologies can be found in our Cookie Policy
+            <p><span class="text-bottom">6. Cross-Border Data Transfers</span></p>
+            <p>Standard contractual clauses are commitments between companies transferring personal data, binding them to protect the privacy and security of your data</p>
+            <p><span class="text-bottom">7. Your Rights</span></p>
+            <p>We want you to be in control of your information, so we have provided you with the following tools: tools and account settings that help you to access, rectify or delete information that you provided to us and that’s associated with your account directly within the service. If you have any question on those tools and settings, please contact our customer care team for help via contact form.</p>
+            <p>You can delete your account by using the corresponding functionality directly on the service.</p>
+            <p>We want you to be aware of your privacy rights. Here are a few key points to remember:</p>
+            <p>Reviewing your information. Applicable privacy laws may give you the right to review the personal information we keep about you (depending on the jurisdiction, this may be called right of access, right of portability or variations of those terms). You can request a copy of your personal information by putting in such a request via contact form.</p>
+            <p>For your protection and the protection of all of our users, we may ask you to provide proof of identity before we can answer the above requests.</p>
+            <p>Keep in mind, we may reject requests for certain reasons, including if the request is unlawful or if it may infringe on trade secrets or intellectual property or the privacy of another user. If you wish to receive information relating to another user, such as a copy of any messages you received from him or her through our service, the other user will have to contact our Privacy Officer to provide their written consent before the information is released.</p>
+            <p>Also, we may not be able to accommodate certain requests to object to the processing of personal information, notably where such requests would not allow us to provide our service to you anymore. </p>
+            <p>In certain countries, including in the European Union, you have a right to lodge a complaint with the appropriate data protection authority if you have concerns about how we process your personal information.</p>
+            <p>Residents of California</p></p>
+            <p>If you are a California resident, you can request a notice disclosing the categories of personal information about you that we have shared with third parties for their direct marketing purposes during the preceding calendar year. To request this notice, please submit your request here. Please allow 30 days for a response. For your protection and the protection of all of our users, we may ask you to provide proof of identity before we can answer such a request.</p>
+            <p><span class="text-bottom">8. How We Protect Your Information</span></p>
+            <p>We work hard to protect you from unauthorized access to or alteration, disclosure or destruction of your personal information. As with all technology companies, although we take steps to secure your information, we do not promise, and you should not expect, that your personal information will always remain secure.
+            <p>We regularly monitor our systems for possible vulnerabilities and attacks and regularly review our information collection, storage and processing practices to update our physical, technical and organizational security measures.
+            <p>We may suspend your use of all or part of the services without notice if we suspect or detect any breach of security. If you believe that your account or information is no longer secure, please notify us immediately via contact form.
+            <span class="text-bottom">9. How Long We Retain Your Information</span></p>
+            <p>We keep your personal information only as long as we need it for legitimate business purposes (as laid out in Section 4) and as permitted by applicable law. To protect the safety and security of our users on and off our services, we implement a safety retention window of three months following account deletion. During this period, account information will be retained although the account will of course not be visible on the services anymore.</p>
+            <p>In practice, we delete or anonymize your information upon deletion of your account (following the safety retention window) or after two years of continuous inactivity, unless:</p>
+            - we must keep it to comply with applicable law (for instance, some “traffic data” is kept for one year to comply with statutory data retention obligations);</p>
+            - we must keep it to evidence our compliance with applicable law (for instance, records of consents to our Terms, Privacy Policy and other similar consents are kept for five years);</p>
+            - there is an outstanding issue, claim or dispute requiring us to keep the relevant information until it is resolved or the information must be kept for our legitimate business interests, such as fraud prevention and enhancing users' safety and security. For example, information may need to be kept to prevent a user who was banned for unsafe behavior or security incidents from opening a new account.</p>
+            <p>Keep in mind that even though our systems are designed to carry out data deletion processes according to the above guidelines, we cannot promise that all data will be deleted within a specific timeframe due to technical constraints.</p>
+            <p><span class="text-bottom">10. Children's Privacy</span></p>
+            <p>Our services are restricted to users who are 18 years of age or older. We do not permit users under the age of 18 on our platform and we do not knowingly collect personal information from anyone under the age of 18. If you suspect that a user is under the age of 18, please use the reporting mechanism available through the website chats.</p>
+            <p><span class="text-bottom">11. Privacy Policy Changes</span></p>
+            <p>Because we’re always looking for new and innovative ways to help you build connections, this policy may change over time. We will notify you before any material changes take effect so that you have time to review the changes.</p>
+            <p><span class="text-bottom">12. How to Contact Us</span>
+            <p>If you have questions about this Privacy Policy, here’s how you can reach us via contact form on our website.</p>
 
-            <h2>Safety and Security</h2>
-            <p>I’m a safety and security section. As part of a privacy policy, in this section you can inform your visitors about how safely you protect their personal information. 
-                Add details such as encryption methods you may use, firewalls employed on your servers, or other security measures you my employ.</p>
- 
-            <p>
-                Your user’s security is of the highest importance to your organization, so take the time to write an accurate and detailed policy.
-                 Use straightforward language to gain their trust and make sure they keep coming back to your site!
-            </p>
         </div>
 
        
--- resources/views/use-terms.blade.php
+++ resources/views/use-terms.blade.php
@@ -14,11 +14,29 @@
     <div class="container">
         <div class="guest-info__text">
            
-​           <p> I’m a terms of use section. I’m a great place to inform your visitors about the nature of your website and how it may be used by visitors. Add details such the type of information and content you publish,
-             or any additional features like taking online payments or collecting emails for a newsletter.</p>
-            <p class="guest-info__paragraph">Transparency helps build trust with your website visitors, so take the time to write an accurate and detailed policy.
-                 Use straightforward language to gain their trust and make sure they keep coming back to your site!
-            </p> 
+            ​   <h2>Summary of Terms</h2> 
+            <p>You must read and agree to our Terms of Use (the “Agreement”) because it forms the binding contract between you and Jet7One. However, we’ve provided this summary for your convenience (with capitalized terms defined in the Agreement).</p>
+
+            <p> <span class="text-bottom"></span>  By using the Service, you agree to this Agreement and our Privacy Policy, and any terms that apply to in website purchase you make.</p>
+
+            <p><span class="text-bottom">Eligibility</span>.  You must be at least 18 to use the Service and not prohibited by law from using the Service. You promise to follow the law and that you’re not a convicted felon or sex offender.</p>
+
+           <p> <span class="text-bottom">Your Account</span>.  You agree to keep your account secure and confidential.</p>
+
+            <p><span class="text-bottom">Modifying and Termination Services</span>.  We may modify the Services at any time or discontinue them altogether. You can terminate your account in Settings <a href="(https://www.jet7.space/settings/profile">(https://www.jet7.space/settings/profile)</a>, and we reserve the right to terminate it for you if you violate this Agreement or if we deem your use of the Service to be inappropriate.</p>
+
+            <p><span class="text-bottom">Safety</span>.  Jet7One is not responsible for the actions of its users or your interactions with them, and we don’t conduct background checks. </p>
+            <p><span class="text-bottom">Rights</span>.  Jet7One grants you the right to use our Service as authorized and permitted by this Agreement. You grant Jet7One the right to display your profile and Content for the limited purpose of Jet7One operating the Service and researching and developing new ones.</p>
+
+            <p><span class="text-bottom">Rules </span>. You agree to abide by when you use Jet7One, such as not soliciting money from other users, harassing other users or using the Service for any illegal purposes.</p>
+
+            <p><span class="text-bottom">Disclaimers </span>. Our Service is provided “as is,” and we disclaim legal liability for the quality, safety, or reliability of our Service.</p>
+
+            <p><span class="text-bottom">Limitation of Liability </span>. To the extent allowed by law, we limit our liability to you for certain types of damages for claims relating to your use of the Service, the conduct of other users and unauthorized access or use of your Content. Our aggregate liability will not exceed the fees you pay us.</p>
+
+            <p><span class="text-bottom">Arbitration </span>. Our Agreement contains a mandatory arbitration clause. You agree that any disputes will be settled by arbitration, and you waive your right to a trial by jury or to participate in a class action. This does not apply to users residing in the EU, Norway or elsewhere where prohibited by law.</p>
+
+            <p><span class="text-bottom">Indemnity </span>. You agree to indemnify us for actions arising out of your use of the Service, your Content or your violation of this Agreement.</p>
  
         </div>
 
