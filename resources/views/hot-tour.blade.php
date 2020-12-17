6aebc0d57c2b9b8bcff0dbbaf4d7f8e5b4799758
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-12-17 16:59:42 +0200

    Додали кнопку реєстрацій для hot-tour

--- resources/views/hot-tour.blade.php
+++ resources/views/hot-tour.blade.php
@@ -23,8 +23,9 @@
 						</div>	
 						<div class="hot-tour__footer-content">
 							<div class="price">€ {{ $tour->price }}</div>
-							<a href="{{ $tour->link() }}" class="btn btn-blue hot-tour__btn">in detail</a>
-							{{-- <a href="#" class="btn btn-green hot-tour__btn">registration</a> --}}
+							{{-- <a href="{{ $tour->link() }}" class="btn btn-blue hot-tour__btn">in detail</a> --}}
+							{{-- <a href="/register" class="btn btn-green hot-tour__btn">registration</a> --}}
+							<a href="/register" class="btn btn-blue hot-tour__btn">registration</a>
 						</div>
 					</div>
 
