fbdeb2b917cec965b59b4751c83fbf11db34bdb2
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-12-24 16:42:18 +0200

    Проблема з картинками в регіоні

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -30,6 +30,7 @@ class RegionController extends Controller
                 if($guide == null){
                     $guide ='There is no  guide for this region yet';
                     $TypeTour = '';
+                    $image = '';
                 }
                 else{
                     $images = json_decode($guide->images);
