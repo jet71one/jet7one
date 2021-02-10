269eeeeaf0640a1716d20d65868490aa0761da55
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2021-02-10 12:35:47 +0200

    Додав контейнер для Регіону

--- resources/views/themes/uikit/regions/region.blade.php
+++ resources/views/themes/uikit/regions/region.blade.php
@@ -10,80 +10,34 @@
     <div class="header__text">
     <h1 class="header__title event-single-title">{{ $region->name}}</h1>
     </div>
-    <div class=" tour-single">
-        <div class="tour__inner">
-            <div class="list-btn">
-                @foreach ($categories as $item)
-                        <?php $countPlace = App\Place::where('location_id', '=' ,$region->id )->where('category_id','=', $item->id)->count() ?>
-                        @if( $countPlace >= '1')
-                            <div class="header__btn btn btn-header">
-                                <a href="{{ route('places.index',['id'=> $item->id,'regID' => $region->id]) }}" class="nav__link">{{ $item->name}}  </a>
-                            </div>   
-                        @endif
-                @endforeach
-                <div class="header__btn btn btn-header">
-                    <a href="{{ route('region.guides',['regID' => $region->id]) }}" class="nav__link">Guides of region</a>
-                </div>  
-                
-            </div>
-            @forelse ($guide as $item)
-            <?php  $collection = collect(json_decode($item->region_id)) ?>
-                @if ($collection->contains($region->id))
-                    <div class="guide__inner">
-                        <div class="guide__slider">
-                            @if( $item->images == null)
-                                <div class="guide__item"><img src="../storage/{{ $item->avatar}}" alt="Avatar image"></div>
-                            @else 
-
-                                @foreach (json_decode($item->images) as $image)
-                                    <div class="guide__item"><img src="../storage/{{ $image}}" alt="{{ $item->name}} image"></div>
-                                @endforeach
+    <div class="container">
+        <div class=" tour-single">
+            <div class="tour__inner">
+                <div class="list-btn">
+                    @foreach ($categories as $item)
+                            <?php $countPlace = App\Place::where('location_id', '=' ,$region->id )->where('category_id','=', $item->id)->count() ?>
+                            @if( $countPlace >= '1')
+                                <div class="header__btn btn btn-header">
+                                    <a href="{{ route('places.index',['id'=> $item->id,'regID' => $region->id]) }}" class="nav__link">{{ $item->name}}  </a>
+                                </div>   
                             @endif
-                
-                        </div>
-                        
-                        <div class="guide__content">
-                            <div class="guide__container">
-                                <div class="guide__body">
-                                    <p class="guide__text">
-                                        <div class="guide__body-title">
-                                        {{$item->name }} 
-                                        </div>
-                                        <h4 class="hot-tour__title guide__type">{{ $TypeTour = App\TypeTour::where('id',"=", $item->type_tour_id)->value('name') }}</h4>
-                                        <p>Languages : {{ $item->lang }}</p>
-                                        <p class="guide__body__text">{{ $item->about }}</p>
-                                        
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                
-                    </div>
-                    @break
-               
-                @endif
-                
-            @empty
-            <p class="guide__body__text">
-                There is no  guide for this region yet
-            </p>
-            @endforelse
-            {{-- {{ dd($guide) }} --}}
-           {{-- @if($guide == 'There is no  guide for this region yet')
-            <p class="guide__body__text">
-                {{ $guide }} 
-            </p>
-           @else 
-                    @if ($guide->role_id == '11')
-                        
-                        @if ($collection->contains($region->id))
+                    @endforeach
+                    <div class="header__btn btn btn-header">
+                        <a href="{{ route('region.guides',['regID' => $region->id]) }}" class="nav__link">Guides of region</a>
+                    </div>  
+                    
+                </div>
+                @forelse ($guide as $item)
+                <?php  $collection = collect(json_decode($item->region_id)) ?>
+                    @if ($collection->contains($region->id))
                         <div class="guide__inner">
                             <div class="guide__slider">
-                                @if( $images == null)
-                                    <div class="guide__item"><img src="../storage/{{ $guide->avatar}}" alt="Avatar image"></div>
+                                @if( $item->images == null)
+                                    <div class="guide__item"><img src="../storage/{{ $item->avatar}}" alt="Avatar image"></div>
                                 @else 
-                                    @foreach ($images as $image)
-                                        <div class="guide__item"><img src="../storage/{{ $image}}" alt="{{ $guide->name}} image"></div>
+    
+                                    @foreach (json_decode($item->images) as $image)
+                                        <div class="guide__item"><img src="../storage/{{ $image}}" alt="{{ $item->name}} image"></div>
                                     @endforeach
                                 @endif
                     
@@ -94,11 +48,11 @@
                                     <div class="guide__body">
                                         <p class="guide__text">
                                             <div class="guide__body-title">
-                                            {{$guide->name }} 
+                                            {{$item->name }} 
                                             </div>
-                                            <h4 class="hot-tour__title guide__type">{{ $TypeTour }}</h4>
-                                            <p>Languages : {{ $guide->lang }}</p>
-                                            <p class="guide__body__text">{{ $guide->about }}</p>
+                                            <h4 class="hot-tour__title guide__type">{{ $TypeTour = App\TypeTour::where('id',"=", $item->type_tour_id)->value('name') }}</h4>
+                                            <p>Languages : {{ $item->lang }}</p>
+                                            <p class="guide__body__text">{{ $item->about }}</p>
                                             
                                         </p>
                                     </div>
@@ -106,55 +60,104 @@
                             </div>
                     
                         </div>
-                        @endif
-
-                        
-                    @else
-                        <div class="guide__inner">
-                            <div class="guide__slider">
-                                @if( $images == null)
-                                    <div class="guide__item"><img src="../storage/{{ $guide->avatar }}" alt="Avatar image"></div>
-                                @else 
-                                    @foreach ($images as $image)
-                                        <div class="guide__item"><img src="../storage/{{ $image}}" alt="{{ $guide->name }} image"></div>
-                                    @endforeach
-                                @endif
-                            </div>
+                        @break
+                   
+                    @endif
+                    
+                @empty
+                <p class="guide__body__text">
+                    There is no  guide for this region yet
+                </p>
+                @endforelse
+                {{-- {{ dd($guide) }} --}}
+               {{-- @if($guide == 'There is no  guide for this region yet')
+                <p class="guide__body__text">
+                    {{ $guide }} 
+                </p>
+               @else 
+                        @if ($guide->role_id == '11')
                             
-                            <div class="guide__content">
-                                <div class="guide__container">
-                                    <div class="guide__body">
-                                        <p class="guide__text">
-                                            <div class="guide__body-title">{{ $guide->name }}</div>
-                                            <h4 class="hot-tour__title guide__type">{{ $TypeTour }}</h4>
-                                            <p>Languages : {{ $guide->lang }}</p>
-                                            <p class="guide__body__text">{{ $guide->about }}</p>
-                                            
-                                        </p>
+                            @if ($collection->contains($region->id))
+                            <div class="guide__inner">
+                                <div class="guide__slider">
+                                    @if( $images == null)
+                                        <div class="guide__item"><img src="../storage/{{ $guide->avatar}}" alt="Avatar image"></div>
+                                    @else 
+                                        @foreach ($images as $image)
+                                            <div class="guide__item"><img src="../storage/{{ $image}}" alt="{{ $guide->name}} image"></div>
+                                        @endforeach
+                                    @endif
+                        
+                                </div>
+                                
+                                <div class="guide__content">
+                                    <div class="guide__container">
+                                        <div class="guide__body">
+                                            <p class="guide__text">
+                                                <div class="guide__body-title">
+                                                {{$guide->name }} 
+                                                </div>
+                                                <h4 class="hot-tour__title guide__type">{{ $TypeTour }}</h4>
+                                                <p>Languages : {{ $guide->lang }}</p>
+                                                <p class="guide__body__text">{{ $guide->about }}</p>
+                                                
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
+                        
                             </div>
-                    
-                        </div>
-                    @endif 
-           @endif --}}
-            
-        </div>
-        {{-- @php $information = array(); @endphp 
-        @forelse ($places as $place)
-            @php array_push($information,"['<a href=>']")
-            @endphp 
-            
-        @endforelse --}}
-       
-        <div id="map" class="map"></div>
-
-      
-        <div class="register__form">
-            <a href="/register" class="btn btn-blue hot-tour__btn">registration</a>
-
+                            @endif
+    
+                            
+                        @else
+                            <div class="guide__inner">
+                                <div class="guide__slider">
+                                    @if( $images == null)
+                                        <div class="guide__item"><img src="../storage/{{ $guide->avatar }}" alt="Avatar image"></div>
+                                    @else 
+                                        @foreach ($images as $image)
+                                            <div class="guide__item"><img src="../storage/{{ $image}}" alt="{{ $guide->name }} image"></div>
+                                        @endforeach
+                                    @endif
+                                </div>
+                                
+                                <div class="guide__content">
+                                    <div class="guide__container">
+                                        <div class="guide__body">
+                                            <p class="guide__text">
+                                                <div class="guide__body-title">{{ $guide->name }}</div>
+                                                <h4 class="hot-tour__title guide__type">{{ $TypeTour }}</h4>
+                                                <p>Languages : {{ $guide->lang }}</p>
+                                                <p class="guide__body__text">{{ $guide->about }}</p>
+                                                
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                        
+                            </div>
+                        @endif 
+               @endif --}}
+                
+            </div>
+            {{-- @php $information = array(); @endphp 
+            @forelse ($places as $place)
+                @php array_push($information,"['<a href=>']")
+                @endphp 
+                
+            @endforelse --}}
+           
+            <div id="map" class="map"></div>
+    
+          
+            <div class="register__form">
+                <a href="/register" class="btn btn-blue hot-tour__btn">registration</a>
+    
+            </div>
         </div>
     </div>
+    
  
 @endsection
 
