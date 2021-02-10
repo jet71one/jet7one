95c95593bbff405bee17acaf65b0c73f1dd00383
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2021-02-10 12:23:34 +0200

    Виправив стилі з зображенями

--- public/themes/uikit/js/app.js
+++ public/themes/uikit/js/app.js
@@ -12987,7 +12987,7 @@ process.umask = function() { return 0; };
 /* 9 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\n        //object-fit: cover\r\n ^\r\n      Invalid CSS after \" }\": expected \"}\", was \"{        //object-f\"\r\n      in E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\assets\\sass\\app.sass (line 613, column 3)\n    at runLoaders (E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\neo-async\\async.js:8077:18)\n    at options.error (E:\\СЕРВАК\\OpenServer\\domains\\wave.loc\\resources\\views\\themes\\uikit\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ })
 /******/ ]);
